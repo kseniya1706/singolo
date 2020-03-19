719afd4b9703e7af9a008a3fc37d41936a5d78e5
Kseniya <55946556+kseniya1706@users.noreply.github.com>
2020-03-19 19:09:42 +0300

    feat: Switching_tabs and Image_Interaction

--- script.js
+++ script.js
@@ -4,3 +4,19 @@ function headerNavigationClick(event) {
    document.querySelector('.nav-choosed').classList.remove('nav-choosed');
    event.target.classList.add('nav-choosed');
 }
+
+
+document.querySelector('.portfolio-tags').addEventListener('mousedown', () => portfolioTagClick(event));
+
+function portfolioTagClick(event) {
+    document.querySelector('.portfolio-tag-choosed').classList.remove('portfolio-tag-choosed');
+    event.target.classList.add('portfolio-tag-choosed');
+ }
+
+
+document.querySelector('.portfolio-projects').addEventListener('mousedown', () => portfolioProjectsClick(event));
+
+function portfolioProjectsClick(event) {
+    document.querySelector('.portfolio-projects-item__pressed').classList.remove('portfolio-projects-item__pressed');
+    event.target.parentElement.classList.add('portfolio-projects-item__pressed');
+ }
